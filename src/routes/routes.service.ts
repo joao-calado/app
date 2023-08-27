3878a24487c43a0b2a04e1fffb7ee5503127c255
joao calado <joaovsscalado@gmail.com>
2023-08-27 18:28:11 +0000

    att apihttp request post and services

--- src/routes/routes.service.ts
+++ src/routes/routes.service.ts
@@ -1,21 +1,45 @@
 import { Injectable } from '@nestjs/common';
 import { CreateRouteDto } from './dto/create-route.dto';
 import { UpdateRouteDto } from './dto/update-route.dto';
+import { PrismaService } from 'src/prisma/prisma/prisma.service';
 
 @Injectable()
 export class RoutesService {
+  constructor(private prismaServide: PrismaService) {}
+
   create(createRouteDto: CreateRouteDto) {
-    return 'This action adds a new route';
+    return this.prismaServide.route.create({
+      data: {
+        name: createRouteDto.name,
+        source: {
+          name: createRouteDto.source_id,
+          location: {
+            lat: 0,
+            lng: 0,
+          },
+        },
+        destination: {
+          name: createRouteDto.destination_id,
+          location: {
+            lat: 0,
+            lng: 0,
+          },
+        },
+        duration: 0,
+        directions: '{}',
+      },
+    });
   }
 
   findAll() {
-    return `This action returns all routes`;
+    return this.prismaServide.route.findMany();
   }
 
   findOne(id: number) {
     return `This action returns a #${id} route`;
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   update(id: number, updateRouteDto: UpdateRouteDto) {
     return `This action updates a #${id} route`;
   }
